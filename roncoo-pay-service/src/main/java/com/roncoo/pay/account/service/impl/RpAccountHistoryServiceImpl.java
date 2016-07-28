9ae01c4d109d0401f941c3f77254d1dc617e3a1e
leslie52 <zenghao2010>
2016-07-28 17:57:13 +0800

    修复查询bug

--- roncoo-pay-service/src/main/java/com/roncoo/pay/account/service/impl/RpAccountHistoryServiceImpl.java
+++ roncoo-pay-service/src/main/java/com/roncoo/pay/account/service/impl/RpAccountHistoryServiceImpl.java
@@ -56,6 +56,7 @@ public class RpAccountHistoryServiceImpl implements RpAccountHistoryService{
 	@Override
 	public PageBean listPage(PageParam pageParam, RpAccountHistory rpAccountHistory) {
 		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("accountNo", rpAccountHistory.getAccountNo());
 		return rpAccountHistoryDao.listPage(pageParam, paramMap);
 	}
 }
