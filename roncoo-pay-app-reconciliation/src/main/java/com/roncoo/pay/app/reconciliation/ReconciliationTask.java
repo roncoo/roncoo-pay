4e4c0f7e3767df6e6c314e8cfbf9563a9082c90f
LYQ <laiyongq@qq.com>
2019-02-21 15:50:33 +0800

    调整对账时间

--- roncoo-pay-app-reconciliation/src/main/java/com/roncoo/pay/app/reconciliation/ReconciliationTask.java
+++ roncoo-pay-app-reconciliation/src/main/java/com/roncoo/pay/app/reconciliation/ReconciliationTask.java
@@ -62,7 +62,7 @@ public class ReconciliationTask {
 	@Autowired
 	private BuildNoService buildNoService;
 
-	@Scheduled(cron = "0 0 1 * * ?")
+	@Scheduled(cron = "0 15 10 * * ?")
 	public void taskRun() {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
