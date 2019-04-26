d9394b6186e0e7c4900b0cd45a701f46c6653697
dinghung <hung12077373@qq.com>
2019-04-27 03:06:15 +0800

    update remote address

--- roncoo-pay-web-sample-shop/src/test/club/dinghui/Test.java
+++ roncoo-pay-web-sample-shop/src/test/club/dinghui/Test.java
@@ -1,11 +1,15 @@
 package club.dinghui;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Test {
     public static void main(String s[]){
 
+        //TomcatEmbeddedServletContainerFactory
 
+        //TomcatServletWebServerFactory
 
     }
 }
