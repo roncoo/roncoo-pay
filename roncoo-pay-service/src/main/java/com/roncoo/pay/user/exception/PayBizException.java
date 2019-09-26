72b93064389cf92e37a6cd44ede6f0cad1babd9a
PeterHu <272750997@qq.com>
2019-09-26 17:10:31 +0800

    支付流程优化，增加花呗分期

--- roncoo-pay-service/src/main/java/com/roncoo/pay/user/exception/PayBizException.java
+++ roncoo-pay-service/src/main/java/com/roncoo/pay/user/exception/PayBizException.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2015-2102 RonCoo(http://www.roncoo.com) Group.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,36 +27,44 @@ import org.apache.commons.logging.LogFactory;
 public class PayBizException extends BizException {
 
     /**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -7163126205323125757L;
 
 	/** 支付方式已存在 **/
     public static final int PAY_TYPE_IS_EXIST = 101;
-    
+
     /** 支付产品已存在 **/
     public static final int PAY_PRODUCT_IS_EXIST = 102;
-    
+
     /** 支付产品已关联数据 **/
     public static final int PAY_PRODUCT_HAS_DATA = 103;
-    
+
     /** 用户支付配置已存在 **/
     public static final int USER_PAY_CONFIG_IS_EXIST = 104;
-    
+
     /** 用户支付配置不存在 **/
     public static final int USER_PAY_CONFIG_IS_NOT_EXIST = 105;
-    
+
     /** 用户支付配置已生效 **/
     public static final int USER_PAY_CONFIG_IS_EFFECTIVE = 106;
-    
+
     /** 支付产品已生效 **/
     public static final int PAY_PRODUCT_IS_EFFECTIVE = 107;
-    
+
     /** 支付产品不存在 **/
     public static final int PAY_PRODUCT_IS_NOT_EXIST = 108;
-    
+
     /** 支付方式不存在 **/
-    public static final int PAY_TYPE_IS_NOT_EXIST = 108;
+    public static final int PAY_TYPE_IS_NOT_EXIST = 109;
+
+    /** 请求参数异常 **/
+    public static final int REQUEST_PARAM_ERR = 110;
+
+    /**
+     * 请求银行异常
+     */
+    public static final int REQUEST_BANK_ERR = 111;
 
     private static final Log LOG = LogFactory.getLog(PayBizException.class);
 
